ef3310d39aacf37e3ee82b3b15959bf68ba168e1
John Andrew Ing <john_andrew_ing@dlsu.edu.ph>
2019-11-24 21:32:06 +0800

    some bean classes

---
+++ app/src/main/java/com/example/lasalliansimulator/Player.java
@@ -0,0 +1,43 @@
+package com.example.lasalliansimulator;
+
+public class Player {
+    private int health;
+    private int social;
+    private int money;
+    private int grades;
+
+    public int getHealth() {
+        return health;
+    }
+
+    public int getSocial() {
+        return social;
+    }
+
+    public void setSocial(int social) {
+        this.social = social;
+    }
+
+    public int getMoney() {
+        return money;
+    }
+
+    public void setMoney(int money) {
+        this.money = money;
+    }
+
+    public int getGrades() {
+        return grades;
+    }
+
+    public void setGrades(int grades) {
+        this.grades = grades;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+
+
+}
